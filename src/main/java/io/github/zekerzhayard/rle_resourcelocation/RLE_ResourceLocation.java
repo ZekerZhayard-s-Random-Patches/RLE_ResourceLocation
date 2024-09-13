c60b08e6ef2e523206980a3bde0a1e0bf9cada8c
ZekerZhayard <trees1000@qq.com>
2024-09-13 22:27:53 +0800

    1.18.2-1.2

--- src/main/java/io/github/zekerzhayard/rle_resourcelocation/RLE_ResourceLocation.java
+++ src/main/java/io/github/zekerzhayard/rle_resourcelocation/RLE_ResourceLocation.java
@@ -1,11 +1,12 @@
 package io.github.zekerzhayard.rle_resourcelocation;
 
+import net.minecraft.ResourceLocationException;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod("rle_resourcelocation")
 public class RLE_ResourceLocation {
     public static void fixValidName(Throwable t) throws Throwable {
-        if (t.getMessage() != null && t.getMessage().endsWith(":DUMMY")) {
+        if (!(t instanceof ResourceLocationException) || (t.getMessage() != null && t.getMessage().endsWith(":DUMMY"))) {
             throw t;
         }
     }
--- src/main/resources/META-INF/transformer.js
+++ src/main/resources/META-INF/transformer.js
@@ -4,22 +4,28 @@ var MethodInsnNode = Java.type("org.objectweb.asm.tree.MethodInsnNode");
 
 function initializeCoreMod() {
     return {
-        "ResourceLocation_<init>": {
+        "ResourceLocation": {
             "target": {
-                "type": "METHOD",
-                "class": "net/minecraft/resources/ResourceLocation",
-                "methodName": "<init>",
-                "methodDesc": "([Ljava/lang/String;)V"
+                "type": "CLASS",
+                "name": "net/minecraft/resources/ResourceLocation"
             },
-            "transformer": function (mn) {
-                var insnList = mn.instructions.toArray();
-                for (var i = 0; i < insnList.length; i++) {
-                    var node = insnList[i];
-                    if (node.getOpcode() === Opcodes.ATHROW) {
-                        mn.instructions.set(node, new MethodInsnNode(Opcodes.INVOKESTATIC, "io/github/zekerzhayard/rle_resourcelocation/RLE_ResourceLocation", "fixValidName", "(Ljava/lang/Throwable;)V", false));
+            "transformer": function (cn) {
+                for (var mnItr = cn.methods.iterator(); mnItr.hasNext();) {
+                    var mn = mnItr.next();
+                    var insnList = mn.instructions.toArray();
+                    var found = false;
+                    for (var i = 0; i < insnList.length; i++) {
+                        var node = insnList[i];
+                        if (node.getOpcode() === Opcodes.NEW && node.desc.equals("net/minecraft/ResourceLocationException")) {
+                            found = true;
+                        }
+                        if (found && node.getOpcode() === Opcodes.ATHROW) {
+                            mn.instructions.set(node, new MethodInsnNode(Opcodes.INVOKESTATIC, "io/github/zekerzhayard/rle_resourcelocation/RLE_ResourceLocation", "fixValidName", "(Ljava/lang/Throwable;)V", false));
+                            found = false;
+                        }
                     }
                 }
-                return mn;
+                return cn;
             }
         }
     }
